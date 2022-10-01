2f58124a3ed7442aac778593a9ec8544693db3d0
Cliford <cliford.mlotshwa@gmail.com>
2022-10-01 16:27:28 +0200

    alert messages

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from "../service/client.service";
 import { Client } from "../model/client";
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap'; 
 
 @Component({
   selector: "app-client-list",
@@ -12,9 +13,11 @@ import { Router } from '@angular/router';
 export class ClientListComponent implements OnInit {
   clients: Observable<Client[]>;
   deleteSuccess = false;
+  closeResult: string;
+  clientName: string; 
+  submitError = false;
 
-  constructor(private clientService: ClientService,
-    private router: Router) {}
+  constructor(private clientService: ClientService, private router: Router, private modalService: NgbModal) {}
 
   ngOnInit() {
     this.reloadData();
@@ -24,22 +27,50 @@ export class ClientListComponent implements OnInit {
     this.clients = this.clientService.getClientsList();
   }
 
-  deleteClient(id: number) {
-    if(confirm("Are you sure to delete this user?")) {
-        
+    deleteClient(id: number) {
         this.clientService.deleteClient(id)
-        .subscribe(
-        data => {
-           this.deleteSuccess = true;
-            setTimeout(function(){
-                window.location.reload();
-            },1000);           
-        },
-        error => console.log(error));
+            .subscribe(
+                data => {
+                    this.deleteSuccess = true;
+                    setTimeout(function(){
+                        window.location.reload();
+                    },1000);           
+                },
+                error => {
+                    console.log(error)
+                    this.submitError = true;
+                }
+            );
     }
-  }
   
   clientDetails(id: number){
     this.router.navigate(['client/details', id]);
   }
+  
+   open(content, id, name) {  
+    this.clientName = name;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {  
+      this.closeResult = `Closed with: ${result}`;  
+      if (result === 'yes') {  
+          this.deleteClient(id);
+      }  
+    }, (reason) => {  
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;  
+    });  
+  }  
+  
+    private getDismissReason(reason: any): string {  
+      if (reason === ModalDismissReasons.ESC) {  
+        return 'by pressing ESC';  
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {  
+        return 'by clicking on a backdrop';  
+      } else {  
+        return `with: ${reason}`;  
+      }  
+    }  
+  
+    closeErrorMessage(){
+        this.submitError = false;
+    }
+  
 }
