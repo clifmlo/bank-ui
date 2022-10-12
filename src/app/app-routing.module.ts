1af45b489319c98f35811a596a60ddbdb35f61eb
Cliford <cliford.mlotshwa@gmail.com>
2022-10-13 00:26:57 +0200

    auth guard: no auth no access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,20 +11,21 @@ import { AccountDetailsComponent } from './account-details/account-details.compo
 import { TransfersComponent } from './transfers/transfers.component';
 import { MenuComponent } from './menu/menu.component';
 import { PasswordComponent } from './password/password.component';
+import { AuthGaurdService } from './service/auth-gaurd.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: '', component: LoginComponent},
-  { path: 'logout', component: LoginComponent},
-  { path: 'clients', component: ClientListComponent },
-  { path: 'client/add', component: CreateClientComponent },
-  { path: 'client/details/:id', component: ClientDetailsComponent },
-  { path: 'new-bank-account/:id', component: CreateBankAccountComponent },
-  { path: 'deposit/:account/:id', component: DepositComponent },
-  { path: 'accounts', component: AccountsComponent },
-  { path: 'account-details/:account', component: AccountDetailsComponent },
-  { path: 'transfer/:type', component: TransfersComponent },
-  { path: 'password/change', component: PasswordComponent },
+  { path: 'logout', component: LoginComponent, canActivate:[AuthGaurdService]},
+  { path: 'clients', component: ClientListComponent, canActivate:[AuthGaurdService]},
+  { path: 'client/add', component: CreateClientComponent, canActivate:[AuthGaurdService]},
+  { path: 'client/details/:id', component: ClientDetailsComponent, canActivate:[AuthGaurdService]},
+  { path: 'new-bank-account/:id', component: CreateBankAccountComponent, canActivate:[AuthGaurdService]},
+  { path: 'deposit/:account/:id', component: DepositComponent, canActivate:[AuthGaurdService]},
+  { path: 'accounts', component: AccountsComponent, canActivate:[AuthGaurdService]},
+  { path: 'account-details/:account', component: AccountDetailsComponent, canActivate:[AuthGaurdService]},
+  { path: 'transfer/:type', component: TransfersComponent, canActivate:[AuthGaurdService]},
+  { path: 'password/change', component: PasswordComponent, canActivate:[AuthGaurdService]},
 ];
 
 @NgModule({
